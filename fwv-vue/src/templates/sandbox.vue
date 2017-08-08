31cda7c630ada72710568264752dd5a7f64393ab
Isaac <isaacxpreston@gmail.com>
2017-08-08 16:48:51 -0400

    saved information to db

--- fwv-vue/src/templates/sandbox.vue
+++ fwv-vue/src/templates/sandbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-on:keydown="check_for_enter">
     <h1>Sandbox</h1>
     <input v-model="name" placeholder="name"></input>
     <input v-model="email" placeholder="email"></input>
@@ -20,12 +20,31 @@
         name: '',
         email: '',
         school: '',
-        status: 'press submit to save to db'
+        status: 'press submit to save to db',
+        default_status: 'press submit to save to db'
       }
     },
     methods: {
+      check_for_enter (e) {
+        if(e.keyCode === 13) {
+          this.submit_login()
+        }
+      },
+      reset_fields () {
+        this.name = ''
+        this.email = ''
+        this.school = ''
+        this.status = this.default_status
+      },
       submit_login () {
         var context = this
+        if(!this.name || !this.email || !this.school) {
+          this.status = 'please fill out all fields'
+          setTimeout(function () {
+            context.reset_fields()
+          }, 2000)
+          return false
+        }
         this.status = 'submitting'
         axios.post(addresses.db_server, {
           name: this.name,
@@ -33,8 +52,10 @@
           school: this.school
         })
         .then((response) => {
-          //do something client side
           context.status = response.data
+          setTimeout(function () {
+            context.reset_fields()
+          }, 2000)
         })
         .catch((error) => {
           context.status = 'error'
