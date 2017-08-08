0a0b237a8d6405bbc6f564ffbf608dbd7791b96a
Isaac <isaacxpreston@gmail.com>
2017-08-08 15:44:06 -0400

    scaffolding client api

--- fwv-vue/src/templates/sandbox.vue
+++ fwv-vue/src/templates/sandbox.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Sandbox</h1>
+    <input v-model="name" placeholder="name"></input>
+    <input v-model="email" placeholder="email"></input>
+    <input v-model="school" placeholder="school"></input>
+    <button v-on:click="submit_login">submit</button>
+    <p>{{status}}</p>
   </div>
 </template>
 
@@ -12,6 +17,27 @@
   export default {
     data () {
       return {
+        name: '',
+        email: '',
+        school: '',
+        status: 'press submit to save to db'
+      }
+    },
+    methods: {
+      submit_login () {
+        var context = this
+        this.status = 'submitting'
+        axios.post('api-address-HERE', {
+          name: this.name,
+          email: this.email,
+          school: this.school
+        })
+        .then((response) => {
+          //do something client side
+        })
+        .catch((error) => {
+          context.status = 'error'
+        })
       }
     },
     mounted () {
